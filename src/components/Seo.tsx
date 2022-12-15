18a8dec92cbdf162be7e4b305f630dc9571ee89a
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-12-15 13:36:03 -0800

    fix the content sizing and spacing on about page

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -16,7 +16,6 @@ const defaultMeta = {
    * No need to be filled, will be populated with openGraph function
    * If you wish to use a normal image, just specify the path below
    */
-
   image: '',
 };
 
