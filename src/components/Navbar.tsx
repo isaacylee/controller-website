4b4962e91c2b4db43848902ed561488bcb67cd76
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-01-01 22:06:32 -0800

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Fragment, useEffect, useState } from 'react';
 
+
 import NewTranslate from '@/components/NewTranslate';
 
 import { ThemeContext } from '@/themeManager';
@@ -119,6 +120,8 @@ export default function Navbar(props: any) {
                       className='block h-8 w-auto dark:hidden'
                       src='/images/logo-elect.png'
                       alt='Kenneth Mejia LA City Controller'
+                       priority={true}
+                    unoptimized={true}
                     />
                     <Image
                       className='hidden h-8 w-auto dark:block'
@@ -126,6 +129,8 @@ export default function Navbar(props: any) {
                       alt='Kenneth Mejia LA City Controller'
                       height={32}
                       width={72}
+                       priority={true}
+                    unoptimized={true}
                     />
                   </Link>
                 </div>
