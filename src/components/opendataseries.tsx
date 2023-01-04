b9ae91cee9e17e38963b56f906efa94d63c1b9fc
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-01-04 13:14:02 -0800

    bruh

--- src/components/opendataseries.tsx
+++ src/components/opendataseries.tsx
@@ -9,6 +9,21 @@ function EachDataButton(props: any) {
           <a href={props.link} target='_blank' rel='noopener noreferrer'>
             <div className='rounded-lg bg-gray-200 px-2 py-2 dark:bg-zinc-800 dark:text-gray-100 '>
               <h3>{props.name}</h3>
+              <p className=''>
+                Last Updated{' '}
+                <span className='font-semibold'>
+                  <time dateTime={props.lastupdated}>
+                    {new Date(props.lastupdated).toLocaleDateString('default', {
+                      year: 'numeric',
+                      month: 'short',
+                      day: 'numeric',
+                    })}
+                  </time>
+                </span>
+              </p>
+              <p className=''>
+                Data Since <span className='font-semibold'>{props.since}</span>
+              </p>
             </div>
           </a>
         </div>
@@ -22,20 +37,30 @@ export default function OpenDataSeries() {
     {
       name: 'Payroll',
       link: 'https://lacity.payroll.finance.socrata.com/#!/year/2022/',
+      tags: ['socrata', 'buggy'],
       lastupdated: '2022-12-30',
+      since: '2013',
     },
     {
       name: 'Checkbook',
       link: 'https://lacity.spending.socrata.com/#!/year/2022/',
+      tags: ['socrata'],
       lastupdated: '2023-01-03',
+      since: '2012',
     },
     {
       name: 'Budget',
       link: 'https://controllerdata.lacity.org/browse?category=Budget',
+      tags: ['socrata'],
+      lastupdated: '2022-11-07',
+      since: '2012',
     },
     {
       name: 'Revenue',
       link: 'https://controllerdata.lacity.org/browse?category=Revenue',
+      tags: ['socrata'],
+      lastupdated: '2022-12-28',
+      since: '2012',
     },
   ];
 
@@ -46,6 +71,9 @@ export default function OpenDataSeries() {
           key={eachlinknum}
           name={eachlink.name}
           link={eachlink.link}
+          lastupdated={eachlink.lastupdated}
+          since={eachlink.since}
+          tags={eachlink.tags}
         />
       ))}
     </div>
