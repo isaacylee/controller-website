ef4bd07d78dbbe212ac15abb733ca7f8347fdeb5
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-01-05 14:42:53 -0800

    fix payroll calendar

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -21,13 +21,19 @@ import { SocialPageSeries } from '@/components/SocialPageSeries';
 interface profilecardprops {
   name: string;
   title: string;
+  picture: string;
 }
 
 function ProfileCard(props: profilecardprops) {
   return (
-    <div className='rounded-lg border border-gray-300 bg-gray-100 px-3 py-2 dark:border-gray-600 dark:bg-zinc-800'>
-      <p className='font-semibold text-black dark:text-white'>{props.name}</p>
-      <p className='text-black dark:text-white'>{props.title}</p>
+    <div className='flex flex-row rounded-lg border border-gray-300 bg-gray-100 dark:border-gray-600 dark:bg-zinc-800'>
+      <div className=''>
+        <Image src={props.picture} alt={'Profile picture of ' + props.name} />
+      </div>
+      <div className='px-3 py-2 '>
+        <p className='font-semibold text-black dark:text-white'>{props.name}</p>
+        <p className='text-black dark:text-white'>{props.title}</p>
+      </div>
     </div>
   );
 }
