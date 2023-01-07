62cc7d718737310e455f8c85fc87b4447fcb4c9b
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-01-07 11:50:00 -0800

    delete excess entries of tableau embed link

--- src/components/cleanthehtmlforprevpages.ts
+++ src/components/cleanthehtmlforprevpages.ts
@@ -1,10 +1,22 @@
 export function cleanthehtml(htmlstring: string) {
-  const cleanedhtmlstring = htmlstring
+  let cleanedhtmlstring = htmlstring
     .replace(
       /https:\/\/(www.)?lacontroller.org\/wp-content\//g,
       'https://wpstaticarchive.lacontroller.io/wp-content/'
     )
     .replace(/https:\/\/(www.)?lacontroller.org\//g, '/');
+
+  const tableaustring =
+    '<script src="https://public.tableau.com/javascripts/api/viz_v1.js"></script>';
+
+  const tableauregex =
+    /<script src="https:\/\/public.tableau.com\/javascripts\/api\/viz_v1.js"><\/script>/gi;
+
+  if (cleanedhtmlstring.includes(tableaustring)) {
+    cleanedhtmlstring = cleanedhtmlstring.replace(tableauregex, '');
+
+    cleanedhtmlstring = tableaustring + cleanedhtmlstring;
+  }
 
   return cleanedhtmlstring;
 }
