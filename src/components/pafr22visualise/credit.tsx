48ead2c532819854af30e7ccbd2b204513fcb103
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-01-24 17:06:15 -0800

    credit rating

--- src/components/pafr22visualise/credit.tsx
+++ src/components/pafr22visualise/credit.tsx
@@ -1,6 +1,36 @@
+import * as d3 from 'd3';
 import * as React from 'react';
 
 export function Credit() {
+  React.useEffect(() => {
+    d3.csv('/csvsforpafr22/5ratingsofthecitydebtbyratingagencies.csv').then(
+      (data: any) => {
+        console.log('credit ratings', data);
+
+        const debttype: any = {};
+
+        data.forEach((d: any) => {
+          console.log(d);
+
+          if (debttype[d["City's Debt Type"]] === undefined) {
+            debttype[d["City's Debt Type"]] = {};
+          }
+
+          if (
+            debttype[d["City's Debt Type"]][d['Rating Agency']] === undefined
+          ) {
+            debttype[d["City's Debt Type"]][d['Rating Agency']] = {};
+          }
+
+          debttype[d["City's Debt Type"]][d['Rating Agency']][d.Year] =
+            d.Rating;
+        });
+
+        console.log('cleaned debt rating', debttype);
+      }
+    );
+  });
+
   return (
     <>
       <div></div>
