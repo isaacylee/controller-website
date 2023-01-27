b975ab7c7a707bd856a09fe9fa0ade9459ad8df7
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-01-26 18:17:31 -0800

    redirects

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,69 +3,9 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/audits-and-reports/:path*',
-        destination: '/audits/:path*',
-        permanent: true,
-      },
-      {
-        source: '/pafr22',
-        destination: '/reports/pafr22',
-        permanent: true,
-      },
-      {
-        source: '/pafr22/',
-        destination: '/reports/pafr22',
-        permanent: true,
-      },
-      {
-        source: '/payroll-calendar/',
-        destination: '/payrollcalendar/',
-        permanent: true,
-      },
-      {
-        source: '/financial-reports/:path',
-        destination: '/reports/:path',
-        permanent: true,
-      },
-      {
-        source: '/audits-and-reviews/:path',
-        destination: '/audits/:path',
-        permanent: true,
-      },
-      {
-        source: '/fraud_hotline/',
-        destination: '/fwa/',
-        permanent: true,
-      },
-      {
-        source: '/report-fraud-waste-and-abuse/',
-        destination: '/fwa/',
-        permanent: true,
-      },
-      {
-        source: '/wtf/',
-        destination: '/fwa/',
-        permanent: true,
-      },
-      {
-        source: '/wp-content/:slug*',
-        destination:
-          'https://wpstaticarchive.lacontroller.io/wp-content/:slug*',
-        permanent: true,
-      },
-      {
-        source: '/4118',
-        destination: 'https://4118.lacontroller.io',
-        permanent: true,
-      },
-    ];
-  },
   // !STARTERCONF Change the siteUrl
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  siteUrl: 'https://tsnext-tw.thcl.dev',
+  siteUrl: 'https://controller.lacity.gov/',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
--- next.config.js
+++ next.config.js
@@ -25,6 +25,66 @@ const securityHeaders = [
 ];
 
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/audits-and-reports/:path*',
+        destination: '/audits/:path*',
+        permanent: true,
+      },
+      {
+        source: '/pafr22',
+        destination: '/reports/pafr22',
+        permanent: true,
+      },
+      {
+        source: '/pafr22/',
+        destination: '/reports/pafr22',
+        permanent: true,
+      },
+      {
+        source: '/payroll-calendar/',
+        destination: '/payrollcalendar/',
+        permanent: true,
+      },
+      {
+        source: '/financial-reports/:path',
+        destination: '/reports/:path',
+        permanent: true,
+      },
+      {
+        source: '/audits-and-reviews/:path',
+        destination: '/audits/:path',
+        permanent: true,
+      },
+      {
+        source: '/fraud_hotline/',
+        destination: '/fwa/',
+        permanent: true,
+      },
+      {
+        source: '/report-fraud-waste-and-abuse/',
+        destination: '/fwa/',
+        permanent: true,
+      },
+      {
+        source: '/wtf/',
+        destination: '/fwa/',
+        permanent: true,
+      },
+      {
+        source: '/wp-content/:slug*',
+        destination:
+          'https://wpstaticarchive.lacontroller.io/wp-content/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/4118',
+        destination: 'https://4118.lacontroller.io',
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
     return [
       {
