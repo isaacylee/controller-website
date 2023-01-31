717441f9193fdd2016225071bfe00e56ce497e15
kenneth-mejia <123411784+kenneth-mejia@users.noreply.github.com>
2023-01-30 16:24:36 -0800

    create animalservices page

---
+++ src/pages/data/animalservices.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+
+import '@/styles/aboutstyles.module.css';
+
+import Layout from '@/components/layout/Layout';
+import Navbar from '@/components/Navbar';
+import Seo from '@/components/Seo';
+
+import { data } from './../data.json';
+
+/**
+ * SVGR Support
+ * Caveat: No React Props Type.
+ *
+ * You can override the next-env if the type is important to you
+ * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
+ */
+
+// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
+// Before you begin editing, follow all comments with `STARTERCONF`,
+// to customize the default configuration.
+
+const kirbybutton =
+  'w-content rounded-full bg-black px-4 py-2 font-bold text-white dark:bg-white dark:text-black';
+
+export default function Animalservices(props: any) {
+  return (
+    <>
+      <Navbar />
+
+      <Layout>
+        {/* <Seo templateTitle='Home' /> */}
+
+        <Seo
+          title='Open Data'
+          description='City of Los Angeles Open Interactive Data.'
+        />
+
+        <div className='mx-2 flex w-full flex-col px-4 py-2 sm:mx-4 md:px-0 lg:mx-auto lg:max-w-3xl xl:max-w-4xl'>
+          
+        </div>
+      </Layout>
+    </>
+  );
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts
+
+  const newaudits = data.map((eachItem: any) => {
+    delete eachItem.textofpage;
+    delete eachItem.pdflink;
+    return eachItem;
+  });
+  return {
+    props: {
+      newaudits,
+    },
+  };
+}
