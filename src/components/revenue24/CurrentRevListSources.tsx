7f68cb83e0727edc15db7fe9ff13f6b01997b275
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-03-09 01:02:28 -0800

    upload major changes

---
+++ src/components/revenue24/CurrentRevListSources.tsx
@@ -0,0 +1,99 @@
+import * as d3 from 'd3';
+import * as React from 'react';
+
+export function CurrentRevListSources(props: any) {
+  const tableRef = React.useRef<any>(null);
+
+  const columns = ['category', 'actual', 'estimated', 'projected'];
+  const headcolumns = [
+    'Name',
+    'Actual FY22',
+    'Estimate FY23',
+    'Projected FY24',
+  ];
+
+  React.useEffect(() => {
+    d3.csv('/csvsrevenueforecast24/gf-current.csv').then((data: any) => {
+      console.log(data);
+
+      const table = d3
+        .select(tableRef.current)
+        .append('table')
+        .attr('class', 'divide-y divide-gray-200 dark:divide-gray-500');
+
+      const thead = table
+        .append('thead')
+        .attr('class', 'bg-gray-50 dark:bg-gray-800');
+      const tbody = table
+        .append('tbody')
+        .attr(
+          'class',
+          'divide-y divide-gray-200 bg-white dark:divide-gray-700 dark:bg-gray-900'
+        );
+
+      thead
+        .append('tr')
+        .selectAll('th')
+        .data(headcolumns)
+        .enter()
+        .append('th')
+        .attr(
+          'class',
+          'px-2 text-wrap max-w-xs tabular-nums align-right text-right'
+        )
+        .text((d: any) => {
+          return d;
+        });
+
+      const rows = tbody
+        .selectAll('tr')
+        .data(data)
+        .enter()
+        .append('tr')
+        .attr('info', (d: any) => JSON.stringify(d))
+        .attr(
+          'class',
+          (d: any) => `${d.category.match(/Total/i) ? 'font-bold' : ''}`
+        );
+
+      const cells = rows
+        .selectAll('td')
+        .data(function (row: any) {
+          return columns.map(function (column) {
+            if (
+              (column === 'actual' ||
+                column === 'estimate' ||
+                column === 'projected') &&
+              parseInt(row[column])
+            ) {
+              return {
+                column: column,
+                value: parseInt(row[column]).toLocaleString('default'),
+              };
+            } else {
+              return { column: column, value: row[column] };
+            }
+          });
+        })
+        .enter()
+        .append('td')
+        .attr('info', (d: any) => JSON.stringify(d))
+        .attr(
+          'class',
+          (d: any) =>
+            `text-sm sm:text-base px-1 md:px-2  ${
+              !Number.isNaN(d.value) ? 'tabular-nums' : ''
+            } `
+        )
+        .text(function (d: any) {
+          return d.value;
+        });
+    });
+  }, []);
+
+  return (
+    <>
+      <div ref={tableRef} id='tableofrevenues'></div>
+    </>
+  );
+}
---
+++ src/components/revenue24/HistoricalRevAll.tsx
@@ -0,0 +1,103 @@
+import * as Plot from '@observablehq/plot';
+import * as d3 from 'd3';
+import * as React from 'react';
+
+export function HistoricalRevAll(props: any) {
+  const generalbox = React.useRef<any>(null);
+  const specialbox = React.useRef<any>(null);
+
+  React.useEffect(() => {
+    d3.csv('/csvsrevenueforecast24/remove-individal-funds-lol.csv').then(
+      (removeIndividalFundsLol: any) => {
+        const removeIndividalFundsLol1 = removeIndividalFundsLol.map(
+          (e: any) => {
+            return {
+              ...e,
+              'Fiscal Year': parseInt(e['Fiscal Year']),
+              'Sum of Amount': parseFloat(e['Sum of Amount']),
+            };
+          }
+        );
+
+        console.log('fetched data', removeIndividalFundsLol1);
+
+        const generateSubPlot = (input: string) => {
+          return Plot.plot({
+            y: {
+              tickFormat: (tick: any) =>
+                d3.format('~s')(tick).replace('G', 'B'),
+              label: 'Revenue',
+            },
+            marks: [
+              Plot.lineY(
+                removeIndividalFundsLol1.filter(
+                  (item: any) =>
+                    item.Type === 'Budget' && item.Category === input
+                ),
+                { x: 'Fiscal Year', y: 'Sum of Amount', stroke: 'Type' }
+              ),
+              Plot.lineY(
+                removeIndividalFundsLol1.filter(
+                  (item: any) =>
+                    item.Type === 'Receipts' &&
+                    item.Category === input &&
+                    item['Fiscal Year'] >= 2022
+                ),
+                {
+                  x: 'Fiscal Year',
+                  y: 'Sum of Amount',
+                  stroke: 'Type',
+                  strokeWidth: 2,
+                  strokeDasharray: '2,6',
+                  defined: (item: any) =>
+                    item['Fiscal Year'] > 2022 && item['Type'] === 'Receipts',
+                }
+              ),
+              Plot.lineY(
+                removeIndividalFundsLol1.filter(
+                  (item: any) =>
+                    item.Type === 'Receipts' &&
+                    item.Category === input &&
+                    item['Fiscal Year'] <= 2022
+                ),
+                { x: 'Fiscal Year', y: 'Sum of Amount', stroke: 'Type' }
+              ),
+            ],
+            color: { scheme: 'set1', type: 'categorical', legend: true },
+          });
+        };
+
+        const generalboxplot = generateSubPlot('General Fund');
+        const specialboxplot = generateSubPlot('Special Funds');
+
+        if (generalbox.current) {
+          console.log('current ref', generalbox.current);
+
+          generalbox.current.innerHTML = '';
+
+          generalbox.current.append(generalboxplot);
+
+          specialbox.current.innerHTML = '';
+
+          specialbox.current.append(specialboxplot);
+
+          const darkstyle = document.createElement('style');
+          darkstyle.innerHTML =
+            '.dark svg[class^="plot-"] {background-color: transparent; color: white;}';
+
+          generalbox.current.append(darkstyle);
+        }
+      }
+    );
+  }, []);
+
+  return (
+    <div>
+      <h3>General Fund Historical Summary</h3>
+      <div className='' ref={generalbox}></div>
+      <h3>Special Funds Historical Summary</h3>
+      <div ref={specialbox}></div>
+      <p></p>
+    </div>
+  );
+}
