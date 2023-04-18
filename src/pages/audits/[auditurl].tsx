fc4c6d4f591f0ca73beee006a835223e2e71af1b
Vartan Arzumanyan <bigballer131@gmail.com>
2023-04-17 19:02:56 -0700

    fixed the rest of the audit links

--- src/pages/audits/[auditurl].tsx
+++ src/pages/audits/[auditurl].tsx
@@ -89,26 +89,6 @@ export default function Audit(props: auditinterface) {
                   View Full Audit
                 </button>
               </Link>
-              <Link href={props.audit.englink}>
-                <button className='rounded-full bg-black px-3 py-1 font-semibold text-white'>
-                  View English Summary
-                </button>
-              </Link>
-              <Link href={props.audit.spanishlink}>
-                <button className='rounded-full bg-black px-3 py-1 font-semibold text-white'>
-                  View Spanish Summary
-                </button>
-              </Link>
-              {/* <Link download={true} href={props.audit.pdflink}>
-                <button className='rounded-full bg-black p-1.5 text-white md:p-2'>
-                  <svg className='l-4 h-4' viewBox='0 0 24 24'>
-                    <path
-                      fill='currentColor'
-                      d='M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z'
-                    />
-                  </svg>
-                </button>
-              </Link> */}
             </div>
 
             <div className='legacycontentgal max-w-3xl'>
