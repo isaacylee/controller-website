e9f13e258333111f76cf09073de731e6a9a1412b
Vartan Arzumanyan <bigballer131@gmail.com>
2023-04-25 13:41:02 -0700

    updated the financial data section with icons

--- src/components/opendataseries.tsx
+++ src/components/opendataseries.tsx
@@ -1,8 +1,13 @@
 import byteSize from 'byte-size';
+import Image from 'next/image';
 import Link from 'next/link';
 import * as React from 'react';
 
-import checkbookinfo from '@/checkbookfilesize.json';
+import checkbookIcon from '@/components/icons/checkbook.png';
+import cityFundsIcon from '@/components/icons/funding.png';
+import payrollIcon from '@/components/icons/payroll.png';
+import revenueIcon from '@/components/icons/revenue.png';
+
 import datainfo from '@/opendatasizes.json';
 
 const ranges = [
@@ -28,6 +33,17 @@ function formatNumber(n: number) {
   return n.toString();
 }
 
+interface EachDataButtonProps {
+  name: string;
+  link: string;
+  lastupdated?: string;
+  since?: string;
+  tags?: string[];
+  size?: string;
+  rowcount?: number;
+  icon?: string;
+}
+
 const kirbybutton =
   'w-content rounded-full bg-black px-4 py-2 font-bold text-white dark:bg-white dark:text-black';
 
@@ -39,8 +55,27 @@ function EachDataButton(props: any) {
       <Link href={props.link}>
         <div className='w-full'>
           <a href={props.link} target='_blank' rel='noopener noreferrer'>
-            <div className='rounded-lg bg-gray-200 px-2 py-2 dark:bg-zinc-800 dark:text-gray-100 '>
-              <h3>{props.name}</h3>
+            <div className='rounded-lg  px-2 py-2 dark:bg-zinc-800 dark:bg-zinc-800 dark:text-gray-100 '>
+              {props.icon && (
+                <div className='relative mb-4 h-16 w-16'>
+                  <div
+                    className='relative mb-4 h-16 w-16'
+                    style={{
+                      backgroundColor: props.darkModeEnabled
+                        ? 'gray'
+                        : 'transparent',
+                    }}
+                  ></div>
+
+                  <Image
+                    src={props.icon}
+                    alt={props.name}
+                    layout='fill'
+                    objectFit='contain'
+                  />
+                </div>
+              )}
+              <h3 className='font-heavy text-lg'>{props.name}</h3>
               <div className='flex w-full flex-row gap-x-1'>
                 {props.tags.includes('socrata') && (
                   <div className='rounded-full bg-gray-100 px-2 py-0.5 text-black dark:bg-zinc-700 dark:text-white'>
@@ -80,24 +115,6 @@ function EachDataButton(props: any) {
                   </time>
                 </span>
               </p>
-              <p className=''>
-                Data Since <span className='font-semibold'>{props.since}</span>
-              </p>
-              <div className='flex flex-row gap-x-2'>
-                <p className=''>
-                  Size{' '}
-                  <span className='font-semibold'>
-                    {byteSizeObj.value}
-                    {byteSizeObj.unit}
-                  </span>
-                </p>
-                <p className=''>
-                  Rows{' '}
-                  <span className='font-semibold'>
-                    {formatNumber(props.rowcount)}
-                  </span>
-                </p>
-              </div>
             </div>
           </a>
         </div>
@@ -110,44 +127,49 @@ export default function OpenDataSeries() {
   const datalinks = [
     {
       name: 'Payroll',
+
       link: 'https://lacity-2.payroll.socrata.com/',
       tags: ['socrata'],
       lastupdated: datainfo.payroll.lastUpdatedAt,
-      since: '2013',
-      size: datainfo.payroll.csvsize,
-      rowcount: datainfo.payroll.rowCount,
+      //  since: '2013',
+      // size: datainfo.payroll.csvsize,
+      //   rowcount: datainfo.payroll.rowCount,
+      icon: payrollIcon,
     },
     {
       name: 'Checkbook',
       link: 'https://lacity.spending.socrata.com/#!/year/2022/',
       tags: ['socrata'],
       lastupdated: datainfo.checkbook.lastUpdatedAt,
-      size: checkbookinfo.checkbooklastupdated[0].filesize,
-      rowcount: datainfo.checkbook.rowCount,
-      since: '2012',
+      //  size: checkbookinfo.checkbooklastupdated[0].filesize,
+      //  rowcount: datainfo.checkbook.rowCount,
+      //  since: '2012',
+      icon: checkbookIcon,
     },
     {
       name: 'All City Funds',
       link: 'https://controllerdata.lacity.org/Audits-and-Reports/All-City-Funds/ej7u-di9z/data',
       tags: ['socrata'],
       lastupdated: datainfo.cityfunds.lastUpdatedAt,
-      since: '2014',
-      size: datainfo.cityfunds.csvsize,
-      rowcount: datainfo.cityfunds.rowCount,
+      //  since: '2014',
+      // size: datainfo.cityfunds.csvsize,
+      // rowcount: datainfo.cityfunds.rowCount,
+      icon: cityFundsIcon,
     },
     {
       name: 'Revenue',
       link: 'https://controllerdata.lacity.org/browse?category=Revenue',
       tags: ['socrata'],
       lastupdated: datainfo.revenue.lastUpdatedAt,
-      rowcount: datainfo.revenue.rowCount,
-      size: datainfo.revenue.csvsize,
-      since: '2012',
+      //  rowcount: datainfo.revenue.rowCount,
+      // size: datainfo.revenue.csvsize,
+      //  since: '2012',
+      icon: revenueIcon,
     },
   ];
 
   return (
-    <div className='grid grid-cols-1 gap-y-4 gap-x-4 sm:grid-cols-2 lg:grid-cols-4'>
+    <div className='dark-bg grid grid-cols-1 gap-y-4 gap-x-4 sm:grid-cols-2 lg:grid-cols-4'>
       {datalinks.map((eachlink: any, eachlinknum: number) => (
         <EachDataButton
           key={eachlinknum}
@@ -158,6 +180,7 @@ export default function OpenDataSeries() {
           tags={eachlink.tags}
           size={eachlink.size}
           rowcount={eachlink.rowcount}
+          icon={eachlink.icon}
         />
       ))}
     </div>
