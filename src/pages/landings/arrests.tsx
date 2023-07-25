26a5800f04ace56018735a89bdb4e9806128f7f3
Vartan Arzumanyan <bigballer131@gmail.com>
2023-07-25 07:41:58 -0700

    spanish translations added to arrest landing page

--- src/pages/landings/arrests.tsx
+++ src/pages/landings/arrests.tsx
@@ -284,12 +284,268 @@ but raises questions about the frequency that children and youth are coming into
  
 </div>
 </center>
- 
- 
-
+  
 <br></br>
+<hr></hr>
+
+<div className='mx-2 flex w-full flex-col px-4 py-2 sm:mx-4 md:px-0 lg:mx-auto lg:max-w-3xl xl:max-w-4xl'>
+          <h2 className='pt-8 pb-4 dark:text-white mobile-left-align'>Mapas y Análisis de Arrestos de LAPD (2019-2022) 
+
+ </h2>
 
  
+<div className="image-container mobile-left-align" style={{ display: "flex", alignItems: "center" }}>
+  <img
+  
+    src="/images/arrests/lapdarrests timelapse GIF.GIF"
+    alt="GRÁFICO: Mapa de tiempo transcurrido mostrando tendencias de arrestos"
+    width="400"
+    height="600"
+    style={{ float: "left", marginRight: "20px" }}
+  />
+  <div style={{ flex: "1" }}>
+    <p className="dark:text-white mobile-left-align" style={{ fontFamily: "Helvetica" }}>
+    Este mapa demuestra ubicaciones y detalles de más de 300,000 arrestos que el
+     Departamento de Policía de Los Ángeles hizo entre 2019 y 2022. Usuarios pueden navegar 
+     el mapa y filtrar por raza, Distrito del Consejo, División de LAPD, y tipo de arrestos.
+      <br />
+      <br />
+      La información fue obtenida del Departamento de Policía de Los Ángeles, pero marca 
+      la primera vez que la información ha sido accesible y cartografiada para el público sin limitaciones.
+
+    </p>
+    <br></br>
+    <br></br>
+    <center> 
+    <a
+  href="https://arrests.lacontroller.io/"
+  target="_blank"
+  style={{
+    backgroundColor: "#41ffca",
+    borderRadius: "8px",
+    border: "2px solid #000000",
+    color: "#000000",
+    // display: "block",
+    fontFamily: "Lato, 'Helvetica Neue', Helvetica, Arial, sans-serif",
+    fontSize: "18px", /* Updated font size */
+    fontWeight: "normal",
+    fontStyle: "normal",
+    padding: "10px 20px", /* Updated padding values */
+    textDecoration: "none",
+    minWidth: "20px",
+    textAlign: "center",
+    direction: "ltr",
+    letterSpacing: "0px"
+  }}
+  rel="noreferrer"
+>
+Abrir el Mapa
+</a></center>
+
+
+  </div>
+</div>
+</div>
+ 
+ 
+ <br></br>
+<center> 
+<div>
+  <h3 className='dark:text-white mobile-left-align' style={{ fontFamily: "Helvetica" }}><b>Resumen y Analisis</b></h3>
+</div>
+</center>
+<br></br>
+<br></br>
+<center> 
+<div>
+  <h3 className='dark:text-white mobile-left-align' style={{ fontFamily: "Helvetica" }}><b>Arrestos por Raza</b></h3>
+</div>
+</center>
+<div className="image-container">
+  <img src="/images/arrests/lapdarrests-race.png" width="400" height="600" alt="A bar chart of LAPD Arrests by Race, 2019-2022. The tallest bar is Hispanic/Latino, at 51%. Next is Black, at 27%, then White at 16%, then Other at 5%. Total arrests for each of the following 4 race categories are not more than 0%: Asian, Pacific Islander, Unknown, and American Indian/Alaskan Native. The Source of Data is LAPD." />
+  <img src="/images/arrests/lapopulation.png" width="400" height="600" alt="A bar chart of Los Angeles Population by Race.  Hispanic/Latino is 48%.  Black is 8%. White is 29%. Other is 0%.  Asian is 12%, Pacific Islander is 0%, 2+ races is 3%, and American Indian/Alaskan Native is 0%. Source of Data is American Community Survey, 2021 5-Year Estimate." />
+</div>
+ 
+<center> 
+<div>
+  
+    <span className='dark:text-white mobile-left-align' style={{ fontFamily: "Helvetica" }}>
+      <p className='mobile-left-align'>Como el mapa refleja, gente morena y negra o afrodescendiente son arrestados en una <br></br>
+      tasa desproporcionada, componiendo un promedio de 78.26% de todos <br></br>los arrestos en los últimos cuatro años (2019-2022), 
+     <br></br> a pesar de ser solo 56% de la población de la ciudad de Los Ángeles, segun los datos del Censo de 2020.
+
+         </p>
+         <br></br>
+    <ul className='mobile-left-align'>
+      {/* <li>Brown and Black people are arrested at a disproportionate rate relative to the overall population  </li> */}
+      <br></br>
+      <li>Gente morena y negra o afrodescendiente = 78.26% arrestos de 2019-2022</li>
+     
+      <li>Gente morena y negra o afrodescendiente = 56% de la población de la ciudad de Los Ángeles (según datos del Censo de 2020)</li>
+      </ul> 
+</span>
+  
+</div>
+ 
+</center>
+<br></br>
+<center> 
+<div>
+  <h3 className='dark:text-white mobile-left-align' style={{ fontFamily: "Helvetica" }}><b>Arrestos por Distrito del Consejo
+</b></h3>
+</div>
+</center>
+<div className="image-container">
+  <img src="/images/arrests/lapdarrestsCD-2019.png" width="400" height="600" alt="A bar chart of LAPD Arrests by Council District, 2019. There are 15 Council Districts. Council District 14 has a much higher number of arrests than any other district, with around 14,000 arrests. There are several other Council Districts with around 7,000 to 9,000 arrests (CDs 1, 6, 8, 9, 13), The rest of the Council Districts have fewer than around 5,000 to 6,000 arrests. CDs 5 and 12 in particular have the fewest number of arrests, at around 2,500 arrests each. Source of data is LAPD." />
+  <img src="/images/arrests/lapdarrestsCD-2020.png" width="400" height="600" alt="A bar chart of LAPD Arrests by Council District, 2020. There are fewer arrests overall compared to 2019. There are 15 Council Districts. Council District 14 has the highest number of arrests, at around 7,500 arrests,  but is also fairly close in number of arrests to districts 8 and 9, which are around 6,000 arrests.  CDs 1, 6, 10, 11, and 13 each have around 5,000 arrests. The rest of the Council Districts have much fewer than 5,000  arrests. CDs 5 and 12 have the fewest number of arrests, at under 2,500 arrests each. Source of data is LAPD." />
+
+</div>
+
+<div className="image-container">
+<img src="/images/arrests/lapdarrestsCD-2021.png" width="400" height="600" alt="A bar chart of LAPD Arrests by Council District, 2021. There are still fewer arrests overall compared to 2019. There are 15 Council Districts. Council Districts  8 and 14 have the highest number of arrests, at around 6,000  arrests,  and are also fairly close in number of arrests to districts 9, and 11 which are also close to 6,000 arrests.  CDs 1, 6, and 13 each have around 5,000 arrests. The rest of the Council Districts havemuch fewer than 5,000  arrests. CDs 5 and 12 again have the fewest number of arrests, at under 2,500 arrests each. Source of data is LAPD." />
+  <img src="/images/arrests/lapdarrestsCD-2022.png" width="400" height="600" alt="A bar chart of LAPD Arrests by Council District, 2022. There are still fewer arrests overall compared to 2019. There are 15 Council Districts. Council Districts 14, 8, and 1 have the highest number of arrests, at around 5,000 to 6,000  arrests,  and are also fairly close in number of arrests to districts 6, 9,  11, and 13 which are also close to 5,000 arrests. The rest of the Council Districts havemuch fewer than 5,000  arrests. CDs 4 and 5 have the fewest number of arrests, at under 2,500 arrests each. Source of data is LAPD. " />
+</div>
+<br />
+<center> 
+<div className='mobile-left-align'>
+ 
+
+    <span className='dark:text-white mobile-left-align' style={{ fontFamily: "Helvetica" }}>
+    Casi cada año, el Distrito del Consejo 14 lidera todos los otros distritos en número de arrestos totales.
+    <br></br>
+    En el 2021, llegó en segundo lugar, después del Distrito del Consejo 8, con una diferencia de solo 3 arrestos.
+
+</span>
+ 
+</div>
+</center>
+<br />
+ 
+<br />
+<center> 
+<div>
+  <h3 className='dark:text-white mobile-left-align' style={{ fontFamily: "Helvetica" }}><b>Tipo de Arresto</b></h3>
+</div>
+</center>
+<center>
+<div className="image-container">
+<img src="/images/arrests/lapdarrests-type2019.png" width="400" height="600" alt="A donut chart of LAPD Arrests by Type, 2019. There are 2 categories: There’s Felony, and there’s Misdemeanor & Infraction. The bigger category is Misdemeanor & Infraction at 55,954, or 62.4%. Felony is 33,663, or 37.6%. Source of Data is LAPD. " />
+  <img src="/images/arrests/lapdarrests-type2020.png" width="400" height="600" alt="A donut chart of LAPD Arrests by Type, 2020. There are 2 categories: There’s Felony, and there’s Misdemeanor & Infraction. The slightly bigger category is Misdemeanor & Infraction at 34,659, or 52.8%. Felony is 31,015, or 47.2%. Source of Data is LAPD. " />
+</div>
+<div className="image-container">
+<img src="/images/arrests/lapdarrests-type2021.png" width="400" height="600" alt="A donut chart of LAPD Arrests by Type, 2021. There are 2 categories: There’s Felony, and there’s Misdemeanor & Infraction. They are almost even.Misdemeanor & Infraction is marginally bigger at 33,179, or 50.4%. Felony is 32,597, or 49.6%. Source of Data is LAPD." />
+  <img src="/images/arrests/lapdarrests-type2022.png" width="400" height="600" alt="A donut chart of LAPD Arrests by Type, 2021. There are 2 categories: There’s Felony, and there’s Misdemeanor & Infraction. They are even.Misdemeanor & Infraction is marginally bigger at 30,431, or 50%. Felony is 30,378, or 50%. Source of Data is LAPD. " />
+</div>
+  
+  </center>
+<br></br>
+<center> 
+<div className='mobile-left-align'>
+ 
+    <span className='dark:text-white' style={{ fontFamily: "Helvetica" }}>
+    Los tipos de arresto de LAPD se dividen en cinco categorías identificadas por LAPD:  <br></br>(1) delito grave,   (2) delito menor, 
+    (3) infracciones, (4) dependiente y (5) otros.
+
+</span>
+ 
+</div>
+</center>
+<br></br>
+<center> 
+<div>
+  
+    <span className='dark:text-white mobile-left-align' style={{ fontFamily: "Helvetica" }}>
+      <p className='mobile-left-align'>Según la ley de California, un delito grave es un delito que puede ser castigado con la 
+      muerte, con encarcelamiento en una prisión estatal o por encarcelamiento en  <br></br>una cárcel del condado bajo [ciertas disposiciones]. Cualquier otro delito y ofensa 
+      pública es un delito menos grave,  <br></br>excepto aquellos delitos que se clasifican como infracciones. 
+      Los delitos menores y las infracciones conllevan diversos grados de consecuencias financieras y de encarcelamiento.
+
+
+          </p>
+ <br></br>
+         {/* <p className='mobile-left-align'>
+         Every other crime or public offense is a misdemeanor except those offenses 
+that are classified as infractions. <br></br>Misdemeanors and
+infractions carry varying degrees of financial and incarceration consequences.
+         </p> */}
+ 
+</span>
+ 
+</div>
+ 
+</center>
+{/* <center> 
+<div className='mobile-left-align'>
+   
+<p className='dark:text-white' style={{fontFamily: "Helvetica"}}> <br></br>
+Under California law, a felony is a crime that is punishable with death, by imprisonment in the 
+state prison, or . . . by imprisonment in a county jail under [certain provisions]. 
+Every other crime or public offense is a misdemeanor except those offenses 
+that are classified as infractions. Misdemeanors and
+infractions carry varying degrees of financial and incarceration consequences.
+
+</p>
+  
+ 
+</div>
+</center> */}
+<br></br>
+<br></br>
+<center> 
+<div>
+ 
+    <p className='dark:text-white mobile-left-align' style={{ fontFamily: "Helvetica" }}>
+    LAPD hace más arrestos por delitos menores e infracciones que por delitos graves.
+
+
+
+    </p>
+   
+</div>
+</center>
+<br></br>
+<center> 
+<div>
+ 
+    <span className='dark:text-white mobile-left-align' style={{ fontFamily: "Helvetica" }}>
+ 
+
+    <ul className='mobile-left-align'>
+      <li className='mobile-left-align'>2019: 55,954 arrestos por delitos menores e infracciones frente a 33,663 arrestos por delitos graves </li>
+      <li className='mobile-left-align'>2020: 34,659 arrestos por delitos menores e infracciones frente a 31,015 arrestos por delitos graves</li>
+      <li className='mobile-left-align'>2021: 33,179 arrestos por delitos menores e infracciones frente a 32 597 arrestos por delitos graves</li>
+      <li className='mobile-left-align'>2022: 30,431 arrestos por delitos menores e infracciones frente a 30 378 arrestos por delitos graves</li>
+      </ul> 
+      <br></br>
+      <div>
+  <h3 className='dark:text-white mobile-left-align' style={{ fontFamily: "Helvetica" }}><b>Arrestos de dependientes</b></h3>
+</div>
+      <p className='dark:text-white mobile-left-align' style={{fontFamily: "Helvetica"}}> <br></br>
+      Según los datos de LAPD, realizan más de 400 arrestos cada año en la categoría de “dependientes”.  <br></br>
+      Esta categoría incluye niños detenidos porque sus padres o guardián han sido acusados ​​de abuso,<br></br>
+      negligencia o peligro, así como niños que se consideran fugitivos o fuera del control de los padres.
+
+
+</p>
+ 
+<p className='dark:text-white mobile-left-align' style={{fontFamily: "Helvetica"}}> <br></br>
+Los datos disponibles no son claros sobre la naturaleza de estas interacciones, pero plantean dudas sobre <br></br>
+la frecuencia con la que los niños y jóvenes entran en contacto con el Departamento de Policía de Los Ángeles.
+
+
+</p>
+<br></br>
+<ul className='mobile-left-align'>
+      <li className='mobile-left-align'>2019: 464 arrestos de dependientes</li>
+      <li className='mobile-left-align'>2020: 429 arrestos de dependientes</li>
+      <li className='mobile-left-align'>2021: 422 arrestos de dependientes</li>
+      <li className='mobile-left-align'>2022: 545 arrestos de dependientes</li>
+      </ul> 
+ 
+</span>
+ 
+</div>
+</center>
     </>
   );
 }
