64f68cbe6be67b198d21eacf922fbf397de32639
na0nie <naonchun@gmail.com>
2023-10-12 17:58:30 -0700

    update analysis

--- src/pages/landings/evictions/index.jsx
+++ src/pages/landings/evictions/index.jsx
@@ -10,7 +10,6 @@ import NoticeType from './NoticeType';
 import Top20Zip from './Top20Zip';
 
 export default function Evictions() {
-
   return (
     <>
       <Navbar />
@@ -18,7 +17,7 @@ export default function Evictions() {
         <center>
           <div className='mx-2 flex w-full flex-col px-4 py-2 sm:mx-4 md:px-0 lg:mx-auto lg:max-w-3xl xl:max-w-4xl'>
             <h1 className='pt-8 pb-4 text-center text-3xl font-bold dark:text-white'>
-              Eviction Notices (February - August 2023)
+              Eviction Notices (February - September 2023)
             </h1>
           </div>
           <div>
@@ -46,30 +45,26 @@ export default function Evictions() {
             className='text-left dark:text-white'
             style={{ fontFamily: 'Helvetica' }}
           >
-            <p className='mb-4'>
-              Last month, we said that there were nearly 40,000 eviction notices
-              from February to July 2023 filed in the City of LA. We have
-              received updated data from the Los Angeles Housing Department
-              (LAHD) through the month of August 2023 and the data reveals that
-              from February to August 2023, LAHD has received a total of{' '}
-              <b>50,000</b> eviction notices. <b>5,575</b> notices were filed
-              for the month of August and <b>4,748</b> are part of updated
-              numbers for February to July from eviction notices received via
-              mail.
+            <p className='mb-2'>
+              [October 2023 Update] We received updated data from the Los
+              Angeles Housing Department (LAHD) through the month of September
+              2023.
             </p>
-            <ul className='mb-10'>
+            <p className='mb-4'>
+              The data reveals that from February to September, LAHD has
+              received a total of <b>59,582</b> eviction notices. <b>5,980</b>{' '}
+              notices were filed for the month of September.
+            </p>
+            <ul className='mb-10 ml-10'>
               <li>
-                &bull; <b>50,000 eviction notices were filed</b>
+                &bull; <b>59,582 eviction notices were filed</b>
               </li>
               <li>
                 &bull;{' '}
-                <b>96.1% of evictions notices were for "non-payment of rent"</b>
+                <b>96% of eviction notices were for "non-payment of rent"</b>
               </li>
               <li>
                 &bull; <b>91% came with a 3-day notice</b>
-              </li>
-              <li>
-                &bull; <b>$186.5M accumulated rent owed</b>
               </li>
             </ul>
           </div>
@@ -111,13 +106,13 @@ export default function Evictions() {
           >
             <p className='mb-4'>
               The table above shows the top 20 Zip Codes with the highest number
-              of eviction notices filed with the City from February to August
+              of eviction notices filed with the City from February to September
               2023.
             </p>
             <p className='mb-4'>
               Units located within <b>90028</b> (Hollywood area) had the highest
-              number of eviction notices filed with <b>3,585</b> followed by{' '}
-              <b>90036</b> (Fairfax area) with <b>2,458</b>.
+              number of eviction notices filed with <b>4,006</b> followed by{' '}
+              <b>90036</b> (Fairfax area) with <b>2,850</b>.
             </p>
           </div>
           {/* <div className='dark:text-white' style={{ fontFamily: 'Helvetica' }}>
@@ -145,7 +140,7 @@ export default function Evictions() {
                 metro area.
               </p>
               <p>
-                <b>6,062</b> eviction notices were filed where the amount of
+                <b>7,262</b> eviction notices were filed where the amount of
                 rent owed is below the Fair Market Rent (FMR) Limit.
               </p>
             </div>
@@ -177,27 +172,49 @@ export default function Evictions() {
             </h4>
             <ul>
               <li className='mb-4 text-left'>
-                &bull; <b>August 1, 2023</b> – rent owed from March 1, 2020 to
-                August 31, 2020 is due. If the Declaration of COVID-19-Related
-                Financial Distress form was returned to the landlord within 15
-                days of rent being due, they cannot be evicted for nonpayment of
-                rent.
+                &bull; Effective <b>February 1, 2024</b>:
               </li>
-              <li className='mb-4 text-left'>
-                &bull; <b>February 1, 2024</b> – rent owed from October 1, 2021
-                to January 31, 2023 is due. If a tenant returned the Declaration
-                of COVID-19-Related Financial Distress form to the landlord
-                within 15 days of rent being due AND paid 25% of rent owed from
-                this period, they cannot be evicted for nonpayment of rent.
-              </li>
-              <li className='mb-4 text-left'>
-                &bull; However, since <b>March 27, 2023</b>, landlords may not
-                evict a tenant who falls behind in rent unless the tenant owes
-                an amount higher than the <b>Fair Market Rent (FMR)</b>. The FMR
-                depends on the bedroom size of the rental unit.
-              </li>
-              <li className='mb-4 text-left'>
-                &bull; If an eviction notice is not provided to LAHD, a tenant
+              <ul className='ml-10'>
+                <li className='mb-4 text-left'>
+                  <p className='mb-4 text-left'>
+                    &#9702; Rent owed from October 1, 2021 to January 31, 2023
+                    is due. If a tenant returned the Declaration of
+                    COVID-19-Related Financial Distress form to the landlord
+                    within 15 days of rent being due AND paid 25% of rent owed
+                    from this period, they cannot be evicted for nonpayment of
+                    rent.
+                  </p>
+                  <p>
+                    However, since March 27, 2023 landlords may not evict a
+                    tenant who falls behind in rent unless the tenant owes an
+                    amount higher than the Fair Market Rent (FMR). The FMR
+                    depends on the bedroom size of the rental unit.
+                  </p>
+                </li>
+                <li className='mb-4 text-left'>
+                  <p className='mb-4'>
+                    &#9702; Unless amended by City Council, annual rent
+                    increases will resume for rental units subject to the City’s
+                    Rent Stabilization Ordinance (RSO) with the allowable rent
+                    increase at 7% starting <b>February 1, 2024</b> through{' '}
+                    <b>June 30, 2024</b>. If the tenant is provided with
+                    electric service and/or gas by the landlord, rent can be
+                    increased an additional 1-2% with a total increase of 8% or
+                    9%.
+                  </p>
+                  <p>
+                    State law requires that the landlord must provide an advance
+                    30-day written notice for all rent increases of less than
+                    10%.
+                  </p>
+                </li>
+                <li className='mb-4 text-left'>
+                  &#9702; Eviction protections for unauthorized occupants or
+                  pets due to COVID-19 expire.
+                </li>
+              </ul>
+              <li className='text-left'>
+                &bull; If an eviction notice is not provided to LAHD, a tenants
                 may raise an affirmative defense in an Unlawful Detainer case.
               </li>
             </ul>
