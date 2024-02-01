2900118b2d840dcc5332590a2ad46b45026171be
vadev <bigballer131@gmail.com>
2024-01-31 16:41:36 -0800

    update evictions analysis page

--- src/pages/landings/evictions/index.jsx
+++ src/pages/landings/evictions/index.jsx
@@ -17,7 +17,7 @@ export default function Evictions() {
         <center>
           <div className='mx-2 flex w-full flex-col px-4 py-2 sm:mx-4 md:px-0 lg:mx-auto lg:max-w-3xl xl:max-w-4xl'>
             <h1 className='pt-8 pb-4 text-center text-3xl font-bold dark:text-white'>
-              Eviction Notices (February - November 2023)
+              Eviction Notices (February - December 2023)
             </h1>
           </div>
           <div>
@@ -77,90 +77,10 @@ export default function Evictions() {
 </b>
               </li>
             </ul>
-            <p className='mb-2'>
-           <b> Top 20 Zip Codes with Highest Number of Eviction Notices</b>
-           <br></br>
-           The table above shows the top 20 Zip Codes with the highest number of eviction notices 
-           filed with the City from February to December 2023.
-           <br></br>
-           Units located within  <b>90028 </b>(Hollywood area) had the highest number of eviction notices 
-           filed with <b>5,156</b> followed by <b>90036</b> (Fairfax area) with <b>3,840</b>.
-
-
-
-            </p>
+         
 
             <br></br>
-            <p className='mb-2'>
-           <b> Fair Market Rent</b>
-           <br></br>
-            
-           The <b>Just Cause Ordinance (JCO)</b> contains a provision where a landlord can evict a 
-           tenant only if the amount due exceeds one month of fair market rent for the Los Angeles metro area.
-
-           <br></br>
-           <b>12,200</b> eviction notices were filed where the amount of rent owed is below the Fair Market Rent (FMR) Limit.
-    </p>
-    <br></br>
-    <p className='mb-2'>
-           <b> Notice Types</b>
-           <br></br>
-            
-           When served with an eviction, tenants are given a deadline to either pay rent owed or move out
-            ("pay or quit") or to fix an issue or move out ("perform or quit"). Other notices will only give a 
-            deadline to move out (such as 30-day notices).
- 
-    </p>
-    <br></br>
-    <p className='mb-2'>
-           <b> Tenant Protection Deadlines</b>
-           <br></br>
-            
-         
-              <li>Effective <b>February 1, 2024:</b></li>
- <br></br>
-              <li style={{ listStyleType: 'circle', fontFamily: 'Arial, sans-serif' }}>Rent owed from October 1, 2021 to January 31, 2023 
-              is due. If a tenant returned the Declaration of COVID-19-Related Financial Distress form to the landlord within 15 days
-               of rent being due AND paid 25% of rent owed from this period, they cannot be evicted for nonpayment of rent.
-
-               However, since <b> March 27, 2023 </b>landlords may not evict a tenant who falls behind in rent unless the 
-               tenant owes an amount higher than the <b> Fair Market Rent (FMR)</b>. The FMR depends on the bedroom size of the 
-               rental unit.
-
-</li>
-<br></br>
-<li style={{ listStyleType: 'circle', fontFamily: 'Arial, sans-serif' }}>As amended by City Council, annual rent increases will 
-resume for rental units subject to the City’s Rent Stabilization Ordinance (RSO) with the allowable rent increase at 4% starting
-<b>February 1, 2024</b> through <b>June 30, 2024</b>.  If the tenant is provided with electric service and/or gas by the landlord, rent can be
- increased an additional 1-2% with a total increase of 5% or 6%.
-	<br></br>
-  <br></br>
-  State law requires that the landlord must provide an advance 30-day written notice for all rent increases of less than 10%.
-  
-             
-
- </li>
-
- <br></br>
- <li style={{ listStyleType: 'circle', fontFamily: 'Arial, sans-serif' }}>Eviction protections will continue for tenants with
-  unauthorized pets obtained during the COVID-19 emergency. Tenants who have not yet told their landlord about the pet will have
-   30 days after the ordinance goes into effect to do so.  This protection does not apply to unauthorized pets that began 
-   residing in the rental unit after 1/31/2024.
-
-  
- </li>
- <br></br>
- <ul>
-  <li>If an eviction notice is not provided to LAHD, a tenant may raise an affirmative defense in an Unlawful Detainer case.</li>
- </ul>
- For future updates on eviction notices, please visit LAHD’s 
-  <a href="https://housing2.lacity.org/residents/renters/eviction-notices-filed"target='_blank'
-     style={{ color: 'white' }} rel="noreferrer"> https://housing2.lacity.org/residents/renters/eviction-notices-filed</a>.
- 
-
           
- 
-    </p>
           </div>
           <div className='dark:text-white' style={{ fontFamily: 'Helvetica' }}>
             <h4 className='mb-4 text-xl font-bold'>
@@ -188,6 +108,7 @@ resume for rental units subject to the City’s Rent Stabilization Ordinance (RS
           <div className='dark:text-white' style={{ fontFamily: 'Helvetica' }}>
             <h4 className='mb-4 text-xl font-bold dark:text-white'>
               <b>Top 20 Zip Codes with Highest Number of Eviction Notices</b>
+         
             </h4>
           </div>
           <div className='mb-12'>
@@ -199,14 +120,12 @@ resume for rental units subject to the City’s Rent Stabilization Ordinance (RS
             style={{ fontFamily: 'Helvetica' }}
           >
             <p className='mb-4'>
-              The table above shows the top 20 Zip Codes with the highest number
-              of eviction notices filed with the City from February to November
-              2023.
+            The table above shows the top 20 Zip Codes with the highest number of eviction notices filed with the City
+            from February to December 2023.
             </p>
             <p className='mb-4'>
-              Units located within <b>90028</b> (Hollywood area) had the highest
-              number of eviction notices filed with <b>4,778</b> followed by{' '}
-              <b>90036</b> (Fairfax area) with <b>3,566</b>.
+            Units located within  <b>90028 </b>(Hollywood area) had the highest number of eviction notices filed 
+            with <b>5,156</b> followed by <b>90036</b> (Fairfax area) with <b>3,840</b>.
             </p>
           </div>
           {/* <div className='dark:text-white' style={{ fontFamily: 'Helvetica' }}>
@@ -228,13 +147,13 @@ resume for rental units subject to the City’s Rent Stabilization Ordinance (RS
             </h4>
             <div className='mb-4 text-left'>
               <p className='mb-4'>
-                The <b>Just Cause Ordinance (JCO)</b> contains a provision where
-                a landlord can evict a tenant only if the amount due{' '}
-                <u>exceeds</u> one month of fair market rent for the Los Angeles
-                metro area.
+              The <b>Just Cause Ordinance (JCO)</b> c contains a provision where a landlord can evict a tenant 
+               only if the amount due{' '}
+               exceeds one month of fair market rent for the Los Angeles metro area.
+
               </p>
               <p>
-                <b>11,461</b> eviction notices were filed where the amount of
+                <b>12,200 </b> eviction notices were filed where the amount of
                 rent owed is below the Fair Market Rent (FMR) Limit.
               </p>
             </div>
@@ -249,10 +168,10 @@ resume for rental units subject to the City’s Rent Stabilization Ordinance (RS
             </h4>
             <div className='mb-4 text-left'>
               <p>
-                When served with an eviction, tenants are given a deadline to
-                either pay rent owed or move out ("pay or quit") or to fix an
-                issue or move out ("perform or quit"). Other notices will only
-                give a deadline to move out (such as 30-day notices).
+              When served with an eviction, tenants are given a deadline to either pay rent owed or move out
+               ("pay or quit") or to fix an issue or move out ("perform or quit"). Other notices will only give a
+                deadline to move out (such as 30-day notices).
+
               </p>
             </div>
           </div>
@@ -271,12 +190,10 @@ resume for rental units subject to the City’s Rent Stabilization Ordinance (RS
               <ul className='ml-10'>
                 <li className='mb-4 text-left'>
                   <p className='mb-4 text-left'>
-                    &#9702; Rent owed from October 1, 2021 to January 31, 2023
-                    is due. If a tenant returned the Declaration of
-                    COVID-19-Related Financial Distress form to the landlord
-                    within 15 days of rent being due AND paid 25% of rent owed
-                    from this period, they cannot be evicted for nonpayment of
-                    rent.
+                    &#9702; Rent owed from October 1, 2021 to January 31, 2023 is due. If a tenant returned the Declaration 
+                    of COVID-19-Related Financial Distress form to the landlord within 15 days of rent being due AND 
+                    paid 25% of rent owed from this period, they cannot be evicted for nonpayment of rent.
+
                   </p>
                   <p>
                     However, since <b>March 27, 2023</b> landlords may not evict a
@@ -303,15 +220,26 @@ resume for rental units subject to the City’s Rent Stabilization Ordinance (RS
                   </p>
                 </li>
                 <li className='mb-4 text-left'>
-                  &#9702; Eviction protections for unauthorized occupants or
-                  pets due to COVID-19 expire.
+                  &#9702; Eviction protections will continue for tenants with unauthorized pets obtained during the COVID-19 emergency.
+                   Tenants who have not yet told their landlord about the pet will have 30 days after the ordinance goes into effect to do so.  This protection does not apply to unauthorized pets that began residing
+                   in the rental unit after 1/31/2024.
                 </li>
               </ul>
               <li className='text-left'>
-                &bull; If an eviction notice is not provided to LAHD, a tenant
-                may raise an affirmative defense in an Unlawful Detainer case.
+                &bull; If an eviction notice is not provided to LAHD, a tenant may raise an affirmative defense in 
+                an Unlawful Detainer case.
+
               </li>
+              
             </ul>
+            <br></br>
+            For future updates on eviction notices, please visit LAHD’s 
+  <a href="https://housing2.lacity.org/residents/renters/eviction-notices-filed"target='_blank'
+     style={{ color: 'white' }} rel="noreferrer"> https://housing2.lacity.org/residents/renters/eviction-notices-filed</a>.
+ 
+ 
+ 
+ 
           </div>
         </center>
       </main>
