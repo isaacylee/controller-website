c6b7c190e2d3dc24714b0d7b0f3abe34047e4781
na0nie <naonchun@gmail.com>
2024-03-11 14:40:18 -0700

    aria-label for bar chart

--- src/pages/landings/cash-for-keys/BuyoutsByYear.jsx
+++ src/pages/landings/cash-for-keys/BuyoutsByYear.jsx
@@ -1,9 +1,8 @@
-"use client";
-import axios from "axios";
-import {Chart, registerables} from "chart.js";
-import { useEffect, useState } from "react";
-import {Bar} from "react-chartjs-2";
-
+'use client';
+import axios from 'axios';
+import { Chart, registerables } from 'chart.js';
+import { useEffect, useState } from 'react';
+import { Bar } from 'react-chartjs-2';
 
 Chart.register(...registerables);
 
@@ -12,76 +11,78 @@ export default function BuyoutsByYear() {
 
   useEffect(() => {
     axios
-    .get('https://api.sheety.co/2996d79e2117ff0d746768a9b29ec03c/tenantBuyoutsAnalysis/tenantBuyoutsByYear')
-    .then((response) => {
+      .get(
+        'https://api.sheety.co/2996d79e2117ff0d746768a9b29ec03c/tenantBuyoutsAnalysis/tenantBuyoutsByYear'
+      )
+      .then((response) => {
         const data = response.data.tenantBuyoutsByYear;
         // console.log("by year", data);
         setBuyoutYears(data);
-    })
-    .catch((error) => {
-        console.error("Error:", error);
-    })
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }, []);
 
-    var data = {
-        labels: buyoutYears.map((x) => x.year),
-        datasets: [
-          {
-            data: buyoutYears.map((x) => x.number),
-            backgroundColor: [
-              "#41ffca",
-            ],
-            borderColor: [
-              "#41ffca",
-            ],
-            borderWidth: 1,
-          },
-        ],
-      };
-    
-      var options = {
-        plugins: {
-          legend: {
-            display: false,
-            labels: {
-              color: "rgb(255, 255, 255)",
-              font: {
-                weight: "bold",
-                size: 12,
-              },
-            },
+  var data = {
+    labels: buyoutYears.map((x) => x.year),
+    datasets: [
+      {
+        data: buyoutYears.map((x) => x.number),
+        backgroundColor: ['#41ffca'],
+        borderColor: ['#41ffca'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  var options = {
+    plugins: {
+      legend: {
+        display: false,
+        labels: {
+          color: 'rgb(255, 255, 255)',
+          font: {
+            weight: 'bold',
+            size: 12,
           },
         },
-        scales: {
-          y: {
-            grid: {
-              display: true,
-              color: "rgba(198, 198, 198, .5)",
-            },
-            ticks: {
-              color: "rgb(255, 255, 255)",
-            },
-            title: {
-                display: true,
-                text: '# of Buyouts',
-              color: "rgb(255, 255, 255)",
-            }
-          },
-          x: {
-            grid: {
-              display: true,
-              color: "rgba(198, 198, 198, .5)",
-            },
-            ticks: {
-              color: "rgb(255, 255, 255)",
-            },
-          },
+      },
+    },
+    aria: {
+      label:
+        'Bar chart of number of tenant buyouts by year from 2019 to 2023. 1209 buyouts in 2019. 1100 buyouts in 2020. 878 buyouts in 2021. 893 buyouts in 2022. 789 buyouts in 2023.',
+    },
+    scales: {
+      y: {
+        grid: {
+          display: true,
+          color: 'rgba(198, 198, 198, .5)',
         },
-      };
-    
-      return (
-        <div className="mt-4 py-4 px-1 sm:px-5 md:px-9 lg:px-20 xl:px-24 bg-zinc-900">
-          <Bar data={data} height={100} width={150} options={options} />
-        </div>
-      );
+        ticks: {
+          color: 'rgb(255, 255, 255)',
+        },
+        title: {
+          display: true,
+          text: '# of Buyouts',
+          color: 'rgb(255, 255, 255)',
+        },
+      },
+      x: {
+        grid: {
+          display: true,
+          color: 'rgba(198, 198, 198, .5)',
+        },
+        ticks: {
+          color: 'rgb(255, 255, 255)',
+        },
+      },
+    },
+  };
+
+  return (
+    <div className='mt-4 bg-zinc-900 py-4 px-1 sm:px-5 md:px-9 lg:px-20 xl:px-24'>
+      <Bar data={data} height={100} width={150} options={options} />
+    </div>
+  );
 }
